f8e038409c0792f2001ee94b75bbac16422c3010
jingx <jingx@ickey.cn>
2022-09-14 08:58:29 +0800

    gin middleware for zaplog

--- gin/go.mod
+++ gin/go.mod
@@ -4,5 +4,9 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.1
-	go.uber.org/zap v1.23.0
+	github.com/zeromicro/go-zero v1.4.0
 )
+
+require af-go-frame v0.0.0-00010101000000-000000000000
+
+replace af-go-frame => ../../af-go-frame
--- gin/main.go
+++ gin/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+    "af-go-frame/core/logx/zapx"
+    "af-go-frame/core/middleware/ginMiddleWare"
     "github.com/gin-gonic/gin"
-    "go.uber.org/zap"
-    )
+    "github.com/zeromicro/go-zero/core/logx"
+    "time"
+)
 
 
 func main(){
 
     r := gin.New()
 
-    logger, _ := zap.NewProduction()
+   // logger, _ := zap.NewProduction()
 
-    r.Use(gin.Logger())
+    writer, err := zapx.NewZapWriter()
+    logx.Must(err)
+    logx.SetWriter(writer)
+
+
+    r.Use(ginMiddleWare.GinZap(writer,time.RFC3339, false))
+   // r.Use(ginMiddleWare.RecoveryWithZap(writer,true))
 
 
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200,gin.H{"message":"pong"})
+    })
+
+    // Example when panic happen.
+    r.GET("/panic", func(c *gin.Context) {
+        panic("An unexpected error happen!")
     })
 
     r.Run()
